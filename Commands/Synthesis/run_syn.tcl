2ca531fc6481d57f46bb8edff4e0531215d61967
Mallikarjun M Yadawad <mallikarjunmyadawad@gmail.com>
2025-03-20 11:16:46 +0530

    Add files via upload

---
+++ Commands/Synthesis/run_syn.tcl
@@ -0,0 +1,23 @@
+set VERILOG s298_Mod
+set TOP_MODULE s298
+mkdir REPORTS
+mkdir OUTPUTS
+read_libs NangateOpenCellLibrary_slow_conditional_ccs.lib
+read_physical -lef NangateOpenCellLibrary.lef
+read_hdl INPUTS/${VERILOG}.v   
+
+elaborate ${TOP_MODULE}
+write_hdl > OUTPUTS/elaborate.v
+read_sdc OUTPUTS/constraints.sdc
+
+syn_generic
+write_hdl > OUTPUTS/syn.v
+syn_map
+write_hdl > OUTPUTS/map.v
+syn_opt
+write_hdl > OUTPUTS/opt.v
+
+report_area > REPORTS/${TOP_MODULE}_area.rep
+report_power > REPORTS/${TOP_MODULE}_power.rep
+report_gates > REPORTS/${TOP_MODULE}_gates.rep
+report_timing > REPORTS/${TOP_MODULE}_timing.rep
