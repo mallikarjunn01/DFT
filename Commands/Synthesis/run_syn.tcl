8e6e66cdfd3d4c327500bceec14fdf6ccfbd2d4d
Mallikarjun <mallikarjunmyadawad@gmail.com>
2025-05-02 15:31:07 +0530

    update

--- Commands/Synthesis/run_syn.tcl
+++ Commands/Synthesis/run_syn.tcl
@@ -1,25 +1,44 @@
-set VERILOG s298 #Sets a variable VERILOG to the value s298
-set TOP_MODULE s298 #Defines the TOP_MODULE variable as s298
-mkdir REPORTS #Creates directories REPORTS
-mkdir OUTPUTS #Creates directories OUTPUTS
- 
-read_libs INPUTS/NangateOpenCellLibrary_slow_conditional_ccs.lib #reads .lib file 
-read_physical -lef INPUTS/NangateOpenCellLibrary.lef  #reads .lef file
-read_hdl INPUTS/${VERILOG}.v  #reads the verilog RTL file
+#Sets a variable VERILOG to the value s298
+set VERILOG s298 
 
-elaborate ${TOP_MODULE} #processes the RTL, checks for syntax errors and infers registers/multiplexers.
-write_hdl > OUTPUTS/elaborate.v #writes the elaborated design to elaborate.v
-read_sdc INPUTS/constraints.sdc #loads timing constraints
+#Defines the TOP_MODULE variable as s298
+set TOP_MODULE s298 
 
-syn_generic #optimizes the design without binding to a specific cell library
+#Creates directories
+mkdir REPORTS  
+mkdir OUTPUTS 
+
+#reads .lib file
+read_libs INPUTS/NangateOpenCellLibrary_slow_conditional_ccs.lib 
+
+#reads .lef file 
+read_physical -lef INPUTS/NangateOpenCellLibrary.lef 
+
+#reads the verilog RTL file 
+read_hdl INPUTS/${VERILOG}.v  
+
+#processes the RTL, checks for syntax errors and infers registers/multiplexers.
+elaborate ${TOP_MODULE} 
+
+#writes the elaborated design to elaborate.v
+write_hdl > OUTPUTS/elaborate.v 
+
+#loads timing constraints
+read_sdc INPUTS/constraints.sdc 
+
+#optimizes the design without binding to a specific cell library
+syn_generic 
 write_hdl > OUTPUTS/syn.v
 
-syn_map  #converts the design into cells available in the target library
+#converts the design into cells available in the target library
+syn_map  
 write_hdl > OUTPUTS/map.v
 
-syn_opt #performs technology-dependent optimizations
+#performs technology-dependent optimizations
+syn_opt 
 write_hdl > OUTPUTS/opt.v
 
+#Reports
 report_area > REPORTS/${TOP_MODULE}_area.rep
 report_power > REPORTS/${TOP_MODULE}_power.rep
 report_gates > REPORTS/${TOP_MODULE}_gates.rep
--- Commands/Timing_Analysis/simple_sta_run.tcl
+++ Commands/Timing_Analysis/simple_sta_run.tcl
@@ -1,20 +1,21 @@
-mkdir REPORTS #Creates directories REPORTS
-mkdir OUTPUTS #Creates directories OUTPUTS
-read_verilog INPUTS/opt.v  # Read the netlist file
+# Read the netlist file
+read_verilog INPUTS/opt.v  
 
-read_lib INPUTS/NangateOpenCellLibrary_slow_conditional_ccs.lib   # Read the library file
+# Read the library file
+read_lib INPUTS/NangateOpenCellLibrary_slow_conditional_ccs.lib   
 
-set_top_module dtmf_recvr_core -ignore_undefined_cell  # Link the design
+# Link the design
+set_top_module dtmf_recvr_core -ignore_undefined_cell  
 
-read_spef INPUTS/corner_worst_CMAX.spef.gz  # Load netlist parasitics
+# Load netlist parasitics
+read_spef INPUTS/corner_worst_CMAX.spef.gz  
 
-read_sdc INPUTS/dtmf_recvr_core.pr.sdc  # Add constraints
-
+# Add constraints
+read_sdc INPUTS/dtmf_recvr_core.pr.sdc 
+ 
 # Run reports
 report_analysis_coverage > REPORTS/Timing_coverage_analysis.rpt 
-
 report_timing -path_type full_clock -max_paths 10 > REPORTS/Timing_report_setupcheck.rpt
 report_timing -early -path_type full_clock -max_paths 10 > REPORTS/Timing_report_holdcheck.rpt
-
 report_constraint -all_violators > REPORTS/Mcrb_constraints.rpt
 
